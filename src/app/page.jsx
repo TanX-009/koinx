a6dfdb659a94958dfa59c32f8aa3aeaac6ef54de
TanX-009 <tanmaymuley009@gmail.com>
2024-03-07 08:27:39 +0530

    fixed vercel building
    moved router.push() to useEffect in /

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,10 +3,13 @@
 import Image from "next/image";
 import styles from "./styles.module.css";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
   const router = useRouter();
 
-  router.push("/bitcoin");
-  return <></>;
+  useEffect(() => {
+    router.push("/bitcoin");
+  });
+  return <div>Redirecting...</div>;
 }
