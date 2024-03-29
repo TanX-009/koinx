6095c3f3f4486d08bc06a330f9ea0765b4b80e97
TanX-009 <tanmaymuley009@gmail.com>
2024-03-29 16:28:47 +0530

    fixed import error in SparklineCard

--- src/app/cryptocurrencies/[symbol]/components/YouMayAlsoLike/components/SparklineCard/index.jsx
+++ src/app/cryptocurrencies/[symbol]/components/YouMayAlsoLike/components/SparklineCard/index.jsx
@@ -4,39 +4,38 @@ import Image from "next/image";
 import Link from "@/components/clickables/Link";
 import SymbolLogo from "@/components/misc/SymbolLogo";
 import Change from "@/components/ui/Change";
-import InnerHTML from "@/systems/ui/InnerHTML";
 import parseCurrency from "@/systems/parseCurrency";
 
 export default function SparklineCard({ coin }) {
-  const value =
-    Math.round(
-      (coin.data.price_change_percentage_24h.usd + Number.EPSILON) * 100,
-    ) / 100;
+	const value =
+		Math.round(
+			(coin.data.price_change_percentage_24h.usd + Number.EPSILON) * 100,
+		) / 100;
 
-  console.log(coin);
+	console.log(coin);
 
-  return (
-    <Link.LowContrast href={`/cryptocurrencies/${coin.id}`}>
-      <div className={styles.widget}>
-        <div className={styles.info}>
-          <div className={styles.title}>
-            <SymbolLogo link={coin.small} name={coin.name} />
-            <p>{coin.symbol}</p>
-            <Change value={value} postfix="%" prefix={false} />
-          </div>
-          <div className={styles.price}>
-            <p>{parseCurrency(coin.data.price, "USD")}</p>
-          </div>
-        </div>
-        <div className={styles.imgContainer}>
-          <Image
-            src={coin.data.sparkline}
-            alt={coin.symbol}
-            fill
-            sizes="(max-width: 30em) 70vw, (max-width: 80em) 40vw, (max-width: 120em) 20vw, 8vw"
-          />
-        </div>
-      </div>
-    </Link.LowContrast>
-  );
+	return (
+		<Link.LowContrast href={`/cryptocurrencies/${coin.id}`}>
+			<div className={styles.widget}>
+				<div className={styles.info}>
+					<div className={styles.title}>
+						<SymbolLogo link={coin.small} name={coin.name} />
+						<p>{coin.symbol}</p>
+						<Change value={value} postfix="%" prefix={false} />
+					</div>
+					<div className={styles.price}>
+						<p>{parseCurrency(coin.data.price, "USD")}</p>
+					</div>
+				</div>
+				<div className={styles.imgContainer}>
+					<Image
+						src={coin.data.sparkline}
+						alt={coin.symbol}
+						fill
+						sizes="(max-width: 30em) 70vw, (max-width: 80em) 40vw, (max-width: 120em) 20vw, 8vw"
+					/>
+				</div>
+			</div>
+		</Link.LowContrast>
+	);
 }
